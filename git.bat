64d2ad57f2229329e1ec1cd2b9d8514ee9233b79
Augustine <qalbarredo@tip.edu.ph>
2026-04-11 20:25:10 +0800

    remove git.bat

--- git.bat
+++
@@ -1,7 +0,0 @@
-echo "# Pre-drivingCrypto"
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Jkllen/Pre-drivingCrypto.git
-git push -u origin main
